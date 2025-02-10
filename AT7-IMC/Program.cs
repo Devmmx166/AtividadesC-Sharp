63ef8cdb835bc274b23002f99217a72434da4713
Devmmx166 <devmmx166@gmail.com>
2025-02-10 15:14:41 -0300

    at1

---
+++ AT7-IMC/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AT7_IMC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o seu peso em kg: ");
+        float peso = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe a sua altura em metros: ");
+        float altura = float.Parse(Console.ReadLine());
+
+        float imc = peso / (altura * altura);
+        Console.WriteLine($"Seu IMC é {imc}.");
+      
+
+    }
+}
